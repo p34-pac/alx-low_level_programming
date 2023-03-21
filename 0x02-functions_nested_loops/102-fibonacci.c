02b49a5d3857edfdc0d5048989b18f16c836569f
p34-pac <peterpaultolulope@gmail.com>
2023-03-21 12:15:39 -0700

    something

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -7,23 +7,6 @@
  */
 int main(void)
 {
-	int i = 0;
-	long j = 1, k = 2;
 
-	while (i < 50)
-	{
-		if (i == 0)
-			printf("%d", j);
-		else if (i == 1)
-			printf(", %d", k);
-		else
-		{
-			k += j;
-			j = k = j;
-			printf(", %ld", k);
-		}
-		++i;
-	}
-	printf("\n");
 	return (0);
 }
