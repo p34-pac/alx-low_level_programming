02de4ef4cf97af7cb87876b2cf806bfed11a1168
p34-pac <peterpaultolulope@gmail.com>
2023-03-21 12:13:30 -0700

    something

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -1,6 +1,5 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include "main.h"
+
 /**
  * main - Entry point
  *
@@ -8,6 +7,23 @@
  */
 int main(void)
 {
+	int i = 0;
+	long j = 1, k = 2;
 
+	while (i < 50)
+	{
+		if (i == 0)
+			printf("%d", j);
+		else if (i == 1)
+			printf(", %d", k);
+		else
+		{
+			k += j;
+			j = k = j;
+			printf(", %ld", k);
+		}
+		++i;
+	}
+	printf("\n");
 	return (0);
 }
