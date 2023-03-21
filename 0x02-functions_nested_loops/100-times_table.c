244290a2f422b8d7b6b9bf82f5d905a915f36710
p34-pac <peterpaultolulope@gmail.com>
2023-03-21 11:51:03 -0700

    something

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -15,8 +15,8 @@ void print_times_table(int n)
 			_putchar('0');
 			for (mult = 1; mult <= n; mult++)
 			{
-				_putchar('.');
-				_putchar('.');
+				_putchar(',');
+				_putchar(' ');
 				prod = num * mult;
 				if (prod <= 99)
 					_putchar(' ');
@@ -25,7 +25,7 @@ void print_times_table(int n)
 				if (prod >= 100)
 				{
 					_putchar((prod / 100) + '0');
-					_putchar((prod / 10) % 10 + '0');
+					_putchar(((prod / 10)) % 10 + '0');
 				}
 				else if (prod <= 99 && prod >= 10)
 				{
