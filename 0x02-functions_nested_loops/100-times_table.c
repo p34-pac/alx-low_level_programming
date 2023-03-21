814f5d9dc4c57580a12846c9827660f9ae419f6e
p34-pac <peterpaultolulope@gmail.com>
2023-03-21 10:45:57 -0700

    something

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -31,14 +31,3 @@ void print_times_table(int n)
 	}
 }
 
-int main(void)
-{
-	print_times_table(3);
-	_putchar('\n');
-	print_times_table(5);
-	_putchar('\n');
-	print_times_table(98);
-	_putchar('\n');
-	print_times_table(12);
-	return (0);
-}
