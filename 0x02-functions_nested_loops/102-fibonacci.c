e224a84e1aceb0d78be4348f280c9390b2b7efcb
p34-pac <peterpaultolulope@gmail.com>
2023-03-21 12:20:17 -0700

    something

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -7,6 +7,21 @@
  */
 int main(void)
 {
+	int fib[50], i;
 
+	fib[0] = 1;
+	fib[1] = 2;
+
+	printf("%d, %d, ", fib[0], fib[1]);
+
+	for (i = 2; i < 50; i++) {
+		fib[i] = fib[i-1] + fib[i-2];
+		printf("%d", fib[i]);
+		if (i < 49) {
+			printf(", ");
+		}
+	}
+
+	printf("\n");
 	return (0);
 }
