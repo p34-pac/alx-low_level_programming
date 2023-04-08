9c0bece72c363cc08e11127caf5f9ffd54fd038a
p34-pac <peterpaultolulope@gmail.com>
2023-04-08 08:27:33 -0700

    something

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -5,7 +5,7 @@
  * main - prints all arguments it receives
  * @argc: number count of arguments
  * @argv: array of arguments
- * return: 0 (SUCCESS)
+ * Return: always 0 (SUCCESS)
  */
 
 int main(int argc, char *argv[])
