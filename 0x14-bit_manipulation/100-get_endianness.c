92bd7ead85e0593b0920b6035c74af0b3ab7627b
p34-pac <peterpaultolulope@gmail.com>
2023-08-03 02:19:22 -0700

    bit manipulation project alx

---
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * get_endianness - check endianness
+ * return: 0 if big endian or 1 if little endian
+ */
+
+int get_endianness(void)
+{
+	int num;
+
+	num = 1;
+	if (*(char *)&num == 1)
+		return (1);
+	else
+		return (0);
+}
+
